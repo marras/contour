8012038e9e73f19dcc2367408733a788e83e885c
Marek Waligorski <marek.waligorski@netguru.pl>
2012-09-30 23:28:36 +0200

    allow floating point numbers in AWK filter, delete 'clicks.dat' if existent

--- preprocess.sh
+++ preprocess.sh
@@ -6,7 +6,7 @@ set -e
 # make a clean, custom header
 cp header.dat czyste.dat
 #only write numbers, only write if all 26 fields are present
-awk '{ if (NF == 26 && $1 ~ /^[0-9]+$/) print $0}' < wszystkie.dat >> czyste.dat
+awk '{ if (NF == 26 && $1 ~ /^[0-9.-+eE]+$/) print $0}' < wszystkie.dat >> czyste.dat
 
 echo "Please enter the three variables you want to plot against"
 echo "or press [Enter] to use last choice:"
@@ -34,5 +34,10 @@ fi
 ruby select_columns.rb $line
 
 ruby check_dups.rb dane_tmp.dat dane.dat --avg
+
+if [ -e 'clicks.dat' ]
+then
+    rm clicks.dat
+fi
 
 python contour.py
