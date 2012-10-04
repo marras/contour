05900b1bf7ef5f9001e757282bebfc7d1f514fc8
Marek Waligorski <marek.waligorski@netguru.pl>
2012-10-04 12:04:49 +0200

    add binding for deleting points (right mouse button)

--- aux.py
+++ aux.py
@@ -33,21 +33,32 @@ class MouseHandler:
     xdatalist = []
     ydatalist = []
 
-    def __init__(self, fig, fp):
+    def __init__(self, fig, fp, redraw_callback):
         self.figure = fig
         self.fp = fp
+        self.redraw_callback = redraw_callback
 
     def on_pick(self, event):
+        print "clicked x = %s and y = %s, button = %d" % (event.xdata, event.ydata, event.button)
         self.event = event
         self.xdatalist.append(event.xdata)
         self.ydatalist.append(event.ydata)
-        print "clicked x = %s and y = %s" % (event.xdata,event.ydata)
-        self.fp.write("%s\t%s\n" % (event.xdata, event.ydata))
+
+        if event.button == 1:
+            self.left_click()
+        elif event.button == 3:
+            self.right_click()
+
+    def left_click(self):
+        self.fp.write("%s\t%s\n" % (self.event.xdata, self.event.ydata))
 
         ax = self.figure.gca()  # get current axis
         ax.hold(True) # overlay plots.
 
         # Plot a red circle where you clicked.
-        ax.plot([event.xdata],[event.ydata],'ro')
+        ax.plot([self.event.xdata],[self.event.ydata],'ro')
 
         self.figure.canvas.draw()  # to refresh the plot.
+
+    def right_click(self):
+        self.redraw_callback(self.event.xdata, self.event.ydata)
